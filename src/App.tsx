08eaeec6466db18261a04f9c75c3364b6bb21639
naomi-rc <naomi.cat182128@gmail.com>
2025-05-21 23:00:02 -0400

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Home from './Pages/Home';
+import HXI from './Pages/HXI';
+import NASATLX from './Pages/NASATLX';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" Component={Home} />
+        <Route path="/hxi" Component={HXI} />
+        <Route path="/nasa-tlx" Component={NASATLX} />
+    </Routes>
+      
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/Pages/HXI.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+
+function HXI() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          HXI QUESTIONNAIRE
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default HXI;
